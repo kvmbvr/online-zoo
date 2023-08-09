d7f504a5ed817bbc1875d6d45c9c8036ea0e56b6
Kambar <kidkambar@gmail.com>
2023-08-09 23:32:35 +0500

    feat: add How component

---
+++ src/components/How/How.jsx
@@ -0,0 +1,14 @@
+const How = () => {
+  return (
+    <section className="how">
+      <div className="how__container">
+        <h2 className="how__title">How it works</h2>
+        <div className="how__images">
+          
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default How;
